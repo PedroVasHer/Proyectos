92504d9b8278a8c2c86efee20ed97f7deccf167a
PedroVasHer <pvasquez242@gmail.com>
2020-01-29 21:02:37 -0500

    finalizacion proyecto

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-type'
 import '../assets/styles/components/CarouselItem.scss'
 import playIcon from '../assets/static/play-icon.png'
 import plusIcon from '../assets/static/plus-icon.png'
@@ -19,4 +20,12 @@ const CarouselItem =({cover,title,year,contentRating,duration})=>( //deben tener
     </div>
 )
 
+CarouselItem.prototype = {
+    cover: PropTypes.string,
+    title: PropTypes.string,
+    year: PropTypes.number,
+    contentRating: PropTypes.string,
+    duration: PropTypes.number
+
+}
 export default CarouselItem
