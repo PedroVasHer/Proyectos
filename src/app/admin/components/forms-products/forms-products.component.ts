56f0c5a81875c3506059e938b210a71da6e8907a
PedroVasHer <pvasquez242@gmail.com>
2020-03-02 21:30:17 -0500

    formulario

--- src/app/admin/components/forms-products/forms-products.component.ts
+++ src/app/admin/components/forms-products/forms-products.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validator, Validators} from '@angular/forms';
 // FormGroup: se tiene varios grupos de variables, varios emails, varios titulos, etc
 // FormBuilder: sirve para crear el grupo rapidamente
+import {ProductsService} from './../../../core/service/products/products.service';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-forms-products',
   templateUrl: './forms-products.component.html',
@@ -10,19 +12,33 @@ import {FormBuilder, FormGroup, Validator, Validators} from '@angular/forms';
 export class FormsProductsComponent implements OnInit {
   form: FormGroup;
   constructor(
-    private formBuilder: FormBuilder
-  ) { 
+    private formBuilder: FormBuilder,
+    private productsService: ProductsService,
+    private router: Router
+  ) {
     this.buildForm(); // por ser una construccion y no datos se pone en el contructor
   }
 
   ngOnInit() {
   }
 
+  saveProduct(event: Event) {
+    event.preventDefault(); // evita que la pagina se recargue cuando se guarda
+    if (this.form.valid) {
+      const product = this.form.value;
+      this.productsService.createProduct(product)
+      .subscribe(newProduct => {
+        console.log(newProduct);
+        this.router.navigate(['./admin/products']); // permite direccionar a otra ruta que se le especifique, en este caso la lista de productos
+      });
+    }
+    // console.log(this.form.value);
+  }
   private buildForm() {
     this.form = this.formBuilder.group({
       id: ['', [Validators.required]],
       title: ['', [Validators.required]],
-      price: [0, [Validators.required]],
+      price: [, [Validators.required]],
       image: [''],
       description: ['', [Validators.required]]
     });
